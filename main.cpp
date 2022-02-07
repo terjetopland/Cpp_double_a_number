69f49293632ed06fa8ee523a5abdd47b833b1fa5
Terje Topland <74875770+terjetopland@users.noreply.github.com>
2022-02-07 09:47:37 +0100

    Done

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    cout << "Enter an integer: \n";
+
+    int numberEntered {};
+    cin >> numberEntered;
+
+    cout << "Double that number is: " << numberEntered * 2 << "\n" << endl;
+    cout << "Triple that number is: " << numberEntered * 3 << "\n" << endl;
+
+
+    return 0;
+}
